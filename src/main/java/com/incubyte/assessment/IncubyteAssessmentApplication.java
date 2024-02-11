e46aab8a091255b63745606418468180e62f6a77
zeji <zeji@zejis-MacBook-Pro-3.local>
2024-02-11 17:14:50 +0530

    Initial Commit

---
+++ src/main/java/com/incubyte/assessment/IncubyteAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.incubyte.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incubyte/assessment/IncubyteAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubyte.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubyteAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
