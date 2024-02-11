62e07739e37709f7a64b7aab05ecb3cdf714f306
zeji <zeji@zejis-MacBook-Pro-3.local>
2024-02-11 17:26:43 +0530

    Calculating sum of given number in a string with a delimiter as commas

--- src/main/java/com/incubyte/assessment/services/StringService.java
+++ src/main/java/com/incubyte/assessment/services/StringService.java
@@ -1,6 +1,10 @@
 package com.incubyte.assessment.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class StringService {
@@ -8,6 +12,15 @@ public class StringService {
         int sum = 0;
         if (numbers == null) {
             throw new IllegalArgumentException("Null input is not allowed");
+        } else if (StringUtils.hasText(numbers)) {
+            String delimiter = ",";
+            List<String> list = Arrays.asList(numbers.split(delimiter));
+            sum = list.stream().filter(number -> {
+                if (number.matches("\\d+"))
+                    return true;
+                else
+                    throw new IllegalArgumentException("The input contains something more than number and delimiter");
+            }).mapToInt(Integer::parseInt).sum();
         }
         return sum;
     }
--- src/test/java/com/incubyte/assessment/services/StringServiceTest.java
+++ src/test/java/com/incubyte/assessment/services/StringServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class StringServiceTest {
@@ -14,5 +15,15 @@ public class StringServiceTest {
     @Test
     void testEmptyStringAsInputShouldReturnZero() {
         assertEquals(0, stringService.add(""));
+    }
+
+    @Test
+    void testCommaSeparatedNumbersInAStringAsInputShouldReturnTheSum() {
+        assertEquals(3, stringService.add("1,2"));
+    }
+
+    @Test
+    void testCommaSeparatedNumbersAndOtherElementsInAStringAsInputShouldReturnError() {
+        assertThrows(IllegalArgumentException.class, () -> stringService.add("1,2,Hi"));
     }
 }
